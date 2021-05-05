e67645d9e9d1debb626395549fa674adb1a35946
MaryamNona <>
2021-05-05 22:28:33 +0000

    added ReadME

---
+++ helloWorld.go
@@ -0,0 +1,13 @@
+// Created by: Maryam Nona
+// Created on: May 2020
+//
+// This program displays, "Hello, World!"
+
+package main
+
+import "fmt"
+
+func main() {
+  // This function displays "Hello, World!"
+  fmt.Println("Hello, World!")
+}
